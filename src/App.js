eaa21e17a5d6c29cac23268b0253c3589d83d612
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2022-04-14 20:01:56 -0300

    Search & Filtering

--- src/App.js
+++ src/App.js
@@ -24,7 +24,15 @@ class App extends React.Component {
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={(event)=>{
-          console.log(event.target.value)
+          console.log(event.target.value);
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          });
+          this.setState(()=>{
+            return {monsters: filteredMonsters}
+          },
+          ()=>{}) 
         }}/>
         {this.state.monsters.map(monster => {
             return(
