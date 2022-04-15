b023788f81feb83cf98c4a916eba2ec7d91fadac
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2022-04-14 23:52:56 -0300

    Optimizations

--- src/App.js
+++ src/App.js
@@ -21,16 +21,25 @@ class App extends React.Component {
         console.log(this.state);
       }) )
   }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      searchField: event.target.value.toLocaleLowerCase()
+    });
+  };
+
   render(){
-    const filteredMonsters = this.state.monsters.filter((monster)=>{
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
     })
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event)=>{
-          console.log(event.target.value);
-          this.setState({searchField: event.target.value.toLocaleLowerCase()});
-        }}/>
+        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
         
         {filteredMonsters.map(monster => {
             return(
