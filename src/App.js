8a6766f291d97e39979781486306ecd5cdadce41
Eduardo Suyama <SUYAMAED@redlink.com.ar>
2022-04-15 23:52:25 -0300

    Finish Touches

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ class App extends React.Component {
     })
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox className='search-box' placeholder='search monsters' onChangeHandler={onSearchChange}/>
         <CardList monsters={filteredMonsters}/>
      </div>
