d34659ae26dea95f9822c2a5332946a25af79b83
Hiro Suyama <hiro.suyama@gamil.com>
2022-05-19 09:50:48 -0300

    Typing React's setState

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { Monster } from '../../App';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
+type CardListProps = {
+    monsters: Monster[];
+};
 
-const CardList = ({monsters}) => (
+const CardList = ({monsters}: CardListProps) => (
     <div className='card-list'>
         {monsters.map(monster=>( 
             <Card monster={monster} key={monster.id}/>
--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import { Monster } from '../../App';
 
 import './card.styles.css';
 
-const Card = ({monster}) => {
+type CardProps = {
+    monster: Monster;
+};
+
+const Card = ({monster}: CardProps) => {
     const {id, name, email} = monster;
     return(
         <div className='card-container' key={id}>
